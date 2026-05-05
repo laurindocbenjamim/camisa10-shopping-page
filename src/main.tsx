e91fc4b3132dbda3655d0a737ad9cd39555b153b
laurindocbenjamim <rocketmc2009@gmail.com>
2026-05-05 11:10:41 +0100

    The app now has proper Router context with nested routes. Try deploying again

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,21 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.tsx'
+import Home from './pages/Home.tsx'
+import Checkout from './pages/Checkout.tsx'
 import './index.css'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Home />} />
+          <Route path="checkout" element={<Checkout />} />
+          <Route path="checkout/:step" element={<Checkout />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
